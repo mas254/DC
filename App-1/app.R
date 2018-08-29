1370003ec0ec7279b283cab262be53e651950241
“mas254” <max.shilling1@googlemail.com>
2018-08-29 11:24:12 +0100

    Shiny

---
+++ App-1/app.R
@@ -0,0 +1,27 @@
+ui <- fluidPage(
+  titlePanel("Hello World!"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 5,
+                  max = 50,
+                  value = 30)
+    ),
+    mainPanel(
+      plotOutput(outputId = "distPlot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    hist(x, breaks = bins, col = "#75AADB", border = "orange",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
+  })
+}
+
+shinyApp(ui = ui, server = server)
