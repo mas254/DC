720f5d08b3f8d830c1ac479a55875384aa085a3c
“mas254” <max.shilling1@googlemail.com>
2018-08-30 09:50:34 +0100

    Shiny

---
+++ Shiny Tutorials/Lesson 4.R
@@ -0,0 +1,8 @@
+# Lesson 4
+
+# You can create reactive output with a two step process.
+
+# Add an R object to your user interface.
+# Tell Shiny how to build the object in the server function.
+# The object will be reactive if the code that builds it calls a widget value.
+
---
+++ Shiny Tutorials/Lesson2.R
@@ -0,0 +1,65 @@
+# Lesson 2
+
+
+ui <- fluidPage(
+  titlePanel("LSOA Data"),
+  sidebarLayout(
+    sidebarPanel("Choose data:"),
+    mainPanel(
+      h3("Download file:")
+      )
+  )
+)
+
+
+server <- function(input, output) {}
+
+
+shinyApp(ui = ui, server = server)
+
+# Now lesson 3 - here also featured text formatting and images in shiny applications - could do CAN here, make look professional
+# rather than just a simple text input box and output csv file.
+
+# Text formatting
+
+ui <- fluidPage(
+  titlePanel("My Shiny App"),
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      p("p creates a paragraph of text."),
+      p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
+      strong("strong() makes bold text."),
+      em("em() creates italicized (i.e, emphasized) text."),
+      br(),
+      code("code displays your text similar to computer code"),
+      div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+      br(),
+      p("span does the same thing as div, but it works with",
+        span("groups of words", style = "color:blue"),
+        "that appear inside a paragraph.")
+    )
+  )
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
+
+# Image insertion - issue here?
+
+ui <- fluidPage(
+  titlePanel("My Shiny App"),
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      img(src = "rstudio.png", height = 140, width = 400)
+    )
+  )
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
+
+# All the code for formatting is shown in this chapter
