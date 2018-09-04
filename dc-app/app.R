95c83c106fc52ac231092b2494f0ae5698a09630
“mas254” <max.shilling1@googlemail.com>
2018-09-04 11:21:03 +0100

    Download button

--- dc-app/app.R
+++ dc-app/app.R
@@ -16,23 +16,22 @@ ui <- fluidPage(
   titlePanel("Indicies of Multiple Deprivation Data"),
   sidebarLayout(
     sidebarPanel(
-      conditionalPanel(
-        'input.dataset === "imd"',
-        checkboxGroupInput("show_vars", "Columns in Indicies of Multiple Deprivation to show:",
-                           names(imd), selected = names(imd))
-      ),
-      conditionalPanel(
-        'input.dataset === "cen"',
-        checkboxGroupInput("show_vars2", "Columns in 2011 Census to show:",
-                           names(cen), selected = names(cen))
-      ),
       helpText("Create a csv file containing information
                from the Indicies of Multiple Deprivation Data
                for Devon postcodes."),
       helpText("Make sure to input the postcode with a space
                before the final three digits (e.g. EX39 1PS or
                EX4 6JL rather than EX391PS or EX46JL)."),
-      fluidRow(column(7, dataTableOutput('dto')))
+      fluidRow(
+        p(downloadButton('downloadData', 'Download'))
+      ),
+      checkboxGroupInput("show_vars2", "Columns in 2011 Census to show:",
+                         names(cen), selected = names(cen)
+      ),
+        checkboxGroupInput("show_vars", "Columns in Indicies of Multiple Deprivation to show:",
+                           names(imd), selected = names(imd)
+      ),
+      fluidRow(column(2, dataTableOutput('dto')))
     ),
     mainPanel(
       tabsetPanel(id = 'dataset',
@@ -46,14 +45,31 @@ server <- function(input, output){
   
   # choose columns to display
   iod = imd[sample(nrow(imd), ), ]
+  
   output$mytable1 <- DT::renderDataTable({
     DT::datatable(iod[, input$show_vars, drop = FALSE])
   })
   
   cens = cen[sample(nrow(cen), ), ]
+  
   output$mytable2 <- DT::renderDataTable({
     DT::datatable(cens[, input$show_vars2, drop = FALSE])
   })
+
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("data-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(output$mytable1, file)
+    }
+  )
+  
+  output$downloadData = downloadHandler('imdfiltered.csv', content = function(file) {
+    s = input$show_vars_rows_all
+    write.csv(imd[s, , drop = FALSE], file)
+  })
+  
   
 }
 
--- test.R
+++ test.R
@@ -12,3 +12,29 @@ server <- function(input,output){
 }
 
 runApp(list(ui=ui,server=server))
+
+
+
+output$downloadData <- downloadHandler(
+  filename = function() {
+    paste(input$dataset, ".csv", sep = "")
+  },
+  content = function(file) {
+    write.csv(datasetInput(), file, row.names = FALSE)
+  })
+
+?write.csv
+names(one)
+one <- read.csv('Files/IofMDFull.csv')
+
+conditionalPanel(
+  'input.dataset === "imd"',
+  checkboxGroupInput("show_vars", "Columns in Indicies of Multiple Deprivation to show:",
+                     names(imd), selected = names(imd))
+),
+conditionalPanel(
+  'input.dataset === "cen"',
+  checkboxGroupInput("show_vars2", "Columns in 2011 Census to show:",
+                     names(cen), selected = names(cen))
+),
+fluidRow(column(7, dataTableOutput('dto'))),
